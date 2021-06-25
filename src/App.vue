8baae6beb10166b2d9e7312361c8bab99b223af0
wwhtrbbtt <64318024+wwhtrbbtt@users.noreply.github.com>
2021-06-26 01:08:05 +0200

    Testing hack to remove tiiny.site logo

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,13 @@
 import Footer from './components/Footer.vue'
 import Navbar from './components/Navbar.vue'
 
+if (location.hostname.includes('.tiiny.site')) {
+  let r =  document.evaluate("/html/body/div[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+  r.singleNodeValue.parentNode.removeChild(r.singleNodeValue)
+}
+
+
+
 export default {
   name: 'App',
   components: {
