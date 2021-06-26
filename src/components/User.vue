f982563a34fd9a5af8d542f410ee04cd6c64df20
wwhtrbbtt <64318024+wwhtrbbtt@users.noreply.github.com>
2021-06-26 12:30:32 +0200

    ✨ Owner_history now showing everywhere | added actual countdown

--- src/components/User.vue
+++ src/components/User.vue
@@ -10,52 +10,40 @@
     <!--When name is loaded -->
 
     <div v-else>
-      <!-- If the name is "illegal" -->
-        <div v-if="isIllegal() && (userData.name == undefined)" class="bg-red-600 p-4 rounded shadow-md grid gap-4 grid-cols-1 md:grid-cols-2"> 
-          <p class="text-3xl p-5  text-white">{{ $route.params.id }} is invalid</p>
-          <p class="text-2xl p-5 text-white">{{ userData.monthlyViews }} Searches (monthly)</p>
-        </div>
+    <!-- If the name is "illegal" -->
+    <div v-if="isIllegal() && (userData.name == undefined)" class="bg-red-600 p-4 rounded shadow-md grid gap-4 grid-cols-1 md:grid-cols-2"> 
+        <p class="text-3xl p-5  text-white">{{ $route.params.id }} is invalid</p>
+        <p class="text-2xl p-5 text-white">{{ userData.monthlyViews }} Searches (monthly)</p>
+    </div>
 
-      <!-- If name is avaible -->
-      <div v-else-if="userData.error == 'Name is avaible'">
-        <div class="bg-green-600 p-4 rounded shadow-md grid gap-4 grid-cols-1 md:grid-cols-2"> 
-          <p class="text-3xl p-5  text-white">{{ $route.params.id }} is avaible*</p>
-          <p class="text-2xl p-5 text-white">{{ (userData.monthlyViews || '1' ) }} Searches (monthly)</p>
+    <!-- If name is avaible -->
+    <div v-else-if="userData.error == 'Name is avaible'">
+      <div class="bg-green-600 p-4 rounded shadow-md grid gap-4 grid-cols-1 md:grid-cols-2"> 
+        <p class="text-3xl p-5  text-white">{{ $route.params.id }} is avaible*</p>
+        <p class="text-2xl p-5 text-white">{{ (userData.monthlyViews || '1' ) }} Searches (monthly)</p>
 
-        </div>
-        <div style="text-align: left;">
-          <p class="text-md">*Unless the name is banned by microsoft, or dropping soon and not cached.</p>
-        </div>
       </div>
-
-      <!-- If name is dropping soon -->
-      <div v-else-if="userData.dropping" class="bg-yellow-600 p-4 rounded shadow-md grid gap-4 grid-cols-1 md:grid-cols-2 items-center justify-center"> 
-        <p class="text-3xl p-5  text-white row-span-2">{{ $route.params.id }} is dropping soon</p>
-      
-        <div class="p-5 items-center justify-center">
-          <p class="text-1xl text-white">{{ userData.monthlyViews }} Searches (monthly)</p>
-          <p class="text-1xl text-white">Dropping in {{ userData.stringDropTime }}</p>
-        </div>
+      <div style="text-align: left;">
+        <p class="text-md">*Unless the name is banned by microsoft, or dropping soon and not cached.</p>
       </div>
+    </div>
 
-      <!-- TODO: fix. Should also appear for illegal names (and probably taken ones too) -->
-      <div v-if="userData.dropping || userData.error == 'Name is avaible'" class="py-8">
-        <!-- Show previous owners -->
-        <p class="text-3xl pb-8">Previous owners</p>
-
-        <p v-if="userData.owner_history.length == 0">
-          No previous owners
-        </p>
-
-        <div v-for="(user, index) in userData.owner_history" v-bind:key="index" class="rounded-md bg-blue-200 dark:bg-gray-700 shadow p-5 mb-5 text-left overflow-auto h-40">
-          <div v-for="(hist, index2) in user.name_history" v-bind:key="index2">
-            <p class="cursor-pointer" v-on:click="toUser(user.name_history[user.name_history.length - index2 -1].name)"><b>
-              {{ user.name_history.length - index2 }}. {{ user.name_history[user.name_history.length - index2 -1].name }}
-            </b></p>
-            <p>{{ toTimestamp(user.name_history[user.name_history.length - index2 -1].changedToAt) }}</p>
-          </div>
-        </div>
+    <!-- If name is dropping soon -->
+    <div v-else-if="userData.dropping" class="bg-yellow-600 p-4 rounded shadow-md grid gap-4 grid-cols-1 md:grid-cols-2 items-center justify-center"> 
+      <p class="text-3xl p-5  text-white row-span-2">{{ $route.params.id }} is dropping soon</p>
+    
+      <div class="p-5 items-center justify-center">
+        <p class="text-1xl text-white">{{ userData.monthlyViews }} Searches (monthly)</p>
+        <!-- <p class="text-1xl text-white">Dropping in {{ userData.stringDropTime }}</p> -->
+        <VueCountdown :time="userData.unixDropTime - Math.floor(Date.now())" v-slot="{ days, hours, minutes, seconds }">
+          <span v-if="days">{{ days }}d </span>
+          <span v-if="hours">{{ hours }}h </span>
+          <span v-if="minutes">{{ minutes }}min </span>
+          {{ seconds }}sec
+        </VueCountdown>
       </div>
+    </div>
+
 
     <!-- If name is taken -->
     <div v-else class="grid gap-4 md:grid-cols-2 grid-cols-1">
@@ -106,7 +94,27 @@
         <p><b>WIP</b></p>
       </div>
     </div>
+
+    <!-- Show previous owners -->
+    <div class="py-8">
+      <p class="text-3xl pb-8">Previous owners</p>
+      <p v-if="userData.owner_history.length == 0">
+        No previous owners
+      </p>
+
+      <div v-for="(user, index) in userData.owner_history" v-bind:key="index" class="rounded-md bg-blue-200 dark:bg-gray-700 shadow p-5 mb-5 text-left overflow-auto max-h-40">
+        <div v-for="(hist, index2) in user.name_history" v-bind:key="index2">
+          <p class="cursor-pointer" v-on:click="toUser(user.name_history[user.name_history.length - index2 -1].name)"><b>
+            {{ user.name_history.length - index2 }}. {{ user.name_history[user.name_history.length - index2 -1].name }}
+          </b></p>
+          <p>{{ toTimestamp(user.name_history[user.name_history.length - index2 -1].changedToAt) }}</p>
+        </div>
+      </div>
     </div>
+
+    </div>
+
+
   </div>
 </template>
 
@@ -114,10 +122,12 @@
 import '../index.css'
 import api from '../apiHandler.js'
 import renderer from "./comps/PlayerModel"
+import VueCountdown from '@chenfengyuan/vue-countdown';
 
 export default {
   components: {
-    renderer
+    renderer,
+    VueCountdown
   },
 
   created() {
@@ -131,6 +141,9 @@ export default {
   
   watch: {
     $route: function(to) {
+      if (to.name == "home") {
+        return
+      }
       this.show = false
       this.userData = {
         "name": "--",
@@ -143,7 +156,7 @@ export default {
         try {
           this.reversedNameHistory = userData.name_history.reverse()
         } catch(e) {
-          console.warn(e)
+          console.warn("error", e)
         }
         this.show = true
       })
